ceee90a4e99ee630f079865763f00caaff91fdc6
arefin22 <shamsularefin500@gmail.com>
2023-11-08 22:42:32 +0600

    Changed The Server

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.pt1xadb.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.m25rjlj.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.m25rjlj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -130,6 +131,8 @@ async function run() {
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
+
+        // Connections % of configured limit has gone above 80
 
         // user Collection Finishes Here
 
