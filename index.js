a00e7b8e593d64dccdc42921c19121455f5dd591
arefin22 <shamsularefin500@gmail.com>
2023-11-09 18:10:52 +0600

    Added Delete Functionality

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
-const corsOption = {
-    origin: ['http://localhost:3000'],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}
+// const corsOption = {
+//     origin: ['http://localhost:3000'],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+// }
 // middlewares
 app.use(cors())
 app.use(express.json())
@@ -41,12 +41,12 @@ async function run() {
 
 
         // auth releted api
-        app.post('/jwt', async (req, res) => {
-            const user = res.body;
-            console.log(user);
-            // const token = jwt.sign(user, 'secret', { expiresIn: "1hr" })
-            res.send(user)
-        })
+        // app.post('/jwt', async (req, res) => {
+        //     const user = res.body;
+        //     console.log(user);
+        //     // const token = jwt.sign(user, 'secret', { expiresIn: "1hr" })
+        //     res.send(user)
+        // })
 
 
         // service Releted api
@@ -59,16 +59,26 @@ async function run() {
             const result = await jobCollection.find().toArray();
             res.send(result)
         })
-        
 
-        app.get('/jobs', async (req, res) => {
-            console.log(req.query.userEmail);
-            let query = {}
-            if(req.query?.userEmail){
-                query = {userEmail:req.query.userEmail}
+
+        app.get('/jobs/:userEmail', async (req, res) => {
+            try {
+                const userEmail = req.params.userEmail;
+                const query = {
+                    userEmail: userEmail,
+                };
+                const result = await jobCollection.find(query).toArray();
+                console.log(result);
+                if (!result) {
+                    res.status(404).send('Item not found');
+                    return;
+                }
+                res.send(result)
             }
-            const result = await jobCollection.find().toArray();
-            res.send(result)
+            catch {
+                console.error('Error:');
+                res.status(500).send('Internal Server Error');
+            }
         })
 
         app.post('/jobs', async (req, res) => {
@@ -126,6 +136,15 @@ async function run() {
         })
 
 
+        app.delete('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await jobCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
 
         // Job Collection Finised
 
@@ -170,7 +189,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
